f536365abaf46ef4dd9c48daea1a67b5c7bf79fd
Longlee68 <129716724+Longlee68@users.noreply.github.com>
2023-05-24 14:07:18 +0700

    nop bai tuan 1

---
+++ demo/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Book;
+import com.example.demo.services.BookService;
+import com.example.demo.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private BookService bookService;
+    @GetMapping
+    public String showAllBooks(Model model){
+        List<Book> books = new ArrayList<>();
+        books  = bookService.getAllBooks();
+        model.addAttribute("books", books);
+        return "book/list";
+    }
+    @GetMapping("/add")
+    public String addBookForm(Model model){
+        model.addAttribute("book", new Book());
+        model.addAttribute("Categories", categoryService.getAllCategories());
+        return "book/add";
+    }
+    @PostMapping("/add")
+    public String addBook(@ModelAttribute("book") Book book){
+        bookService.addBook(book);
+        return "redirect:/books";
+    }
+}
